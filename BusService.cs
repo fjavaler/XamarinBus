8b9a2e81a6faac2ed89e49e90196e97e04bc0294
Fred Javalera <frederickjavalera@gmail.com>
2019-08-10 22:47:13 -0600

    init commit

---
+++ BusService.cs
@@ -0,0 +1,36 @@
+﻿namespace XamarinBus
+{
+  using MongoDB.Driver;
+  using MongoDB.Bson;
+  using System;
+  using System.Threading.Tasks;
+  //using MongoDB.Driver.Builders;
+  //using MongoDB.Driver.GridFS;
+  using MongoDB.Driver.Linq;
+
+  class BusService
+  {
+    public ObjectId Id { get; set; }
+    public string Name { get; set; }
+
+    static void Main(string[] args)
+    {
+      var client = new MongoClient();
+      var database = client.GetDatabase("FredsDB");
+      var collection = database.GetCollection<BsonDocument>("FredsCollection");
+      var document = new BsonDocument
+      {
+          { "name", "MongoDB" },
+          { "type", "Database" },
+          { "count", 1 },
+          { "info", new BsonDocument
+          {
+              { "x", 203 },
+              { "y", 102 }
+          }}
+      };
+      collection.InsertOne(document);
+      
+    }
+  }
+}
